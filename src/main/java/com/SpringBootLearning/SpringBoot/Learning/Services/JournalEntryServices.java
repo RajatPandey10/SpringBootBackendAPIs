e021234b242f4a60ddc8ec7231a2ad6fd76f15f1
RajatPandey10 <pandeyrajat9971@gmail.com>
2025-10-05 00:08:05 +0530

    add logging to UserServices for better debugging

--- src/main/java/com/SpringBootLearning/SpringBoot/Learning/Services/JournalEntryServices.java
+++ src/main/java/com/SpringBootLearning/SpringBoot/Learning/Services/JournalEntryServices.java
@@ -4,6 +4,8 @@ import com.SpringBootLearning.SpringBoot.Learning.Entity.JournalEntry;
 import com.SpringBootLearning.SpringBoot.Learning.Entity.User;
 import com.SpringBootLearning.SpringBoot.Learning.Repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +22,7 @@ public class JournalEntryServices {
 
     @Autowired
     private UserServices userServices;
+
 
     @Transactional
     public void saveNewEntry(JournalEntry journalEntry, String userName){
--- src/main/java/com/SpringBootLearning/SpringBoot/Learning/Services/UserServices.java
+++ src/main/java/com/SpringBootLearning/SpringBoot/Learning/Services/UserServices.java
@@ -2,7 +2,10 @@ package com.SpringBootLearning.SpringBoot.Learning.Services;
 
 import com.SpringBootLearning.SpringBoot.Learning.Entity.User;
 import com.SpringBootLearning.SpringBoot.Learning.Repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,6 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Component
+@Slf4j
 public class UserServices {
 
     @Autowired
@@ -27,9 +31,18 @@ public class UserServices {
 
 
     public void saveNewUser(User user){
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRoles(Arrays.asList("USER"));
-        userRepository.save(user);
+        try{
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            user.setRoles(Arrays.asList("USER"));
+            userRepository.save(user);
+        }catch (Exception e){
+            log.error("error occurred for {} :",user.getUserName(),e);
+//            logger.warn("Hey I am logger");
+//            logger.error("Hey I am logger");
+//            logger.trace("Hey I am logger");
+//            logger.debug("Hey I am logger");
+        }
+
     }
 
     public void saveAdmin(User user){
